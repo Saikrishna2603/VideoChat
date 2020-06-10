a483f2f463f8b3eedfa9b3a32472691078810ead
saikrishna <msaikrishna2603@gmail.com>
2020-06-10 21:21:05 +0530

    Adding video chat

---
+++ js/config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+var SAMPLE_SERVER_BASE_URL = 'https://tokboxproject.herokuapp.com/';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+var API_KEY = '46776424';
+var SESSION_ID = '1_MX40Njc3NjQyNH5-MTU5MTgwMzEzNDQ3Mn5qT3RlOWxrNGhxR1FCd3N5ak9kQVpCMTh-fg';
+var TOKEN = 'T1==cGFydG5lcl9pZD00Njc3NjQyNCZzaWc9Y2I4NjEzMTMxMTEwNDMyZDNmNDVjMjlkMjAxNTAwODRlNjI2YzQ0YjpzZXNzaW9uX2lkPTFfTVg0ME5qYzNOalF5Tkg1LU1UVTVNVGd3TXpFek5EUTNNbjVxVDNSbE9XeHJOR2h4UjFGQ2QzTjVhazlrUVZwQ01UaC1mZyZjcmVhdGVfdGltZT0xNTkxODAzMTUxJm5vbmNlPTAuNzQxOTQ4MDg1NTc2ODQ5MiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTk0Mzk1MTUxJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
